cf8eed1048ff780a5cb78d164eb31c6312834862
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-03-05 18:03:16 +0100

    Support importing app as node module

---
+++ app.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const cliCursor = require('cli-cursor');
+const parseArgs = require('minimist');
+const playercast = require('./lib/index');
+const terminal = require('./lib/terminal');
+
+cliCursor.hide();
+
+const opts = {
+	boolean: [
+		'quiet', 'cec-alt-remote', 'cec-force-switch', 'disable-cec',
+		'listen', 'create-service', 'remove-service'
+	],
+	string: ['subs', 'name', 'player', 'port', 'cec-end-hdmi'],
+	alias: { q: 'quiet', s: 'subs', n: 'name', p: 'player' },
+	default: { p: (process.platform === 'win32') ? 'vlc' : 'mpv' },
+	unknown: (option) => onUnknown(option)
+};
+
+const args = process.argv.slice(2);
+const argv = parseArgs(args, opts);
+init();
+
+function init()
+{
+	if(!checkArgvStrings())
+		return terminal.showHelp();
+
+	const app = playercast(argv);
+}
+
+function onUnknown(option)
+{
+	if(!option.startsWith('-')) return;
+
+	terminal.showHelp();
+	process.exit(1);
+}
+
+function checkArgvStrings()
+{
+	if(
+		argv.hasOwnProperty('cec-end-hdmi')
+		&& (isNaN(argv['cec-end-hdmi']) || argv['cec-end-hdmi'] < 0)
+	)
+		return false;
+
+	if(
+		argv.hasOwnProperty('port')
+		&& (isNaN(argv.port) || argv.port < 1 || argv.port > 65535)
+	)
+		return false;
+
+	for(var key of opts.string)
+		if(argv[key] === '') return false;
+
+	return true;
+}
